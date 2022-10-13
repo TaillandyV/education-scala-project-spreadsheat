ecd6954e5ecee9f46af43f21515c51d98be35971
AdamGouja <adam.gouja@gmail.com>
2022-10-13 11:54:43 +0200

    add test cell

--- src/test/scala/spreadsheat/CellSuite.scala
+++ src/test/scala/spreadsheat/CellSuite.scala
@@ -3,9 +3,14 @@ package spreadsheat
 import spreadsheat.Cell
 
 class CellSuite extends munit.FunSuite {
-  test("error cell shopuld return #error") {
+  test("error cell should return #error") {
     val cell = Cell.Error
 
     assertEquals(cell.eval, "#ERROR")
+  }
+  test("Cell is a number") {
+    val cell = 124
+
+    assertEquals(Cell.Number(cell).eval, "124.0")
   }
 }
