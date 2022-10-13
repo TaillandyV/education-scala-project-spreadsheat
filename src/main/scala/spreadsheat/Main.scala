023c63236ecc44ec096e6068a5ee108871bcee86
khalila <adel.khalil@edu.esiee.fr>
2022-10-13 11:16:32 +0200

    create front spreadsheet

--- src/main/scala/spreadsheat/Main.scala
+++ src/main/scala/spreadsheat/Main.scala
@@ -4,3 +4,26 @@ package spreadsheat
 def main = println("Hello world")
 
 
+package org.stairwaybook.scells
+
+import swing._
+
+class Spreadsheet(val height: Int, val width: Int)
+  extends ScrollPane {
+
+  val table = new Table(height, width) {
+    rowHeight = 25
+    autoResizeMode = Table.AutoResizeMode.Off
+    showGrid = true
+    gridColor = new java.awt.Color(150, 150, 150)
+  }
+
+  val rowHeader =
+    new ListView((0 until height) map (_.toString)) {
+      fixedCellWidth = 30
+      fixedCellHeight = table.rowHeight
+    }
+
+  viewportView = table
+  rowHeaderView = rowHeader
+}
