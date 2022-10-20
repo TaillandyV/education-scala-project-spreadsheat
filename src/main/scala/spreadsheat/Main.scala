640293584438c091520f9ee06d1d5c42cd280906
TaillandyV <valentin.taillandy@edu.esiee.fr>
2022-10-20 18:50:39 +0200

    Keep the engine running, added the command EXIT to quit the engine, modified the function for addValue

--- src/main/scala/spreadsheat/Main.scala
+++ src/main/scala/spreadsheat/Main.scala
@@ -1,11 +1,40 @@
 package spreadsheat
+import scala.io.StdIn.readLine
 
 @main
 def main(): Unit = {
-  val spreadSheet = Spreadsheet.empty(10, 10)
-  spreadSheet.show
-  println("\n\n")
-  val updatedSpreadSheet = spreadSheet.add(1, 1, 20.0)
 
-  updatedSpreadSheet.show
+  val colTable = (0 until 26).map(index => (index + 'A').toChar.toString -> index).toMap
+
+  var running = true
+
+  val spreadSheet = Spreadsheet.empty(3, 3)
+  spreadSheet.show
+
+
+  while(running){
+    println("\n\n")
+    println("Que voulez-vous faire ?")
+    val input = readLine()
+    input match {
+      case "EXIT" => running = false
+      case _ => val updatedSpreadSheet = addValue(input,colTable,spreadSheet)
+        updatedSpreadSheet.show
+    }
+  }
+
+
+
+}
+
+def addValue(input:String,colTable:Map[String,Int],spreadSheet: Spreadsheet): Spreadsheet = {
+  val command = input.split("=")
+  val location = command(0)
+  val value = command(1)
+
+  val col = colTable.getOrElse("[0-9]+".r.split(location)(0), 0)
+  val row = "[A-Z]+".r.split(location)(1).toInt
+
+
+  spreadSheet.add(row, col, value.toFloat)
 }
