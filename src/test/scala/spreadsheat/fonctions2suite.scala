02165d152e9fcf24da1c0dd23b6c29e915d8e30a
khalila <adel.khalil@edu.esiee.fr>
2022-10-23 15:26:41 +0200

    Function compare and equals + test with empty or with text cell and cell number

--- src/test/scala/spreadsheat/fonctions2suite.scala
+++ src/test/scala/spreadsheat/fonctions2suite.scala
@@ -1,23 +1,69 @@
 package spreadsheat
 import munit.Clue.generate
 import spreadsheat.Cell
+
 import math.Ordered.orderingToOrdered
 import math.Ordering.Implicits.infixOrderingOps
 import munit.Clue.generate
-import spreadsheat.Cell.stringToBooleen
+import spreadsheat.Cell.{isCellEmpty, isCellNum, stringToBooleen}
 
 def equals(l1 : Cell, l2: Cell): Boolean = l1.equals(l2)
 
+def inf(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 < a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
 
-def inf(l1 : Cell.Number , l2: Cell.Number ): Boolean = {
-  val a1= println(Cell.stringToCell(l1))
-  val a2= println(Cell.stringToCell(l2))
-  def inf(a1 : Int , a2: Int ): Boolean = a1 < a2
-}
+def supp(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 > a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
 
+def infouegale(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 <= a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
 
-
-//def < (v1 : ) : Boolean = (this compare that) < this
+def suppouegale(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 >= a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
 
 class Functions2Suite extends munit.FunSuite {
 
@@ -25,19 +71,54 @@ class Functions2Suite extends munit.FunSuite {
     val s1 = Cell.Number(124)
     val s2 = Cell.Number(124)
     val result = s1.equals(s2)
-    println(result)
-    assertEquals(result, true)
-  }
-
-  test("inf") {
-    val s1 = Cell.Number(124)
-    val s2 = Cell.Number(124)
-    val result = inf(s1,s2)
-    println(result)
+    println(Cell.Booleen(result))
     assertEquals(result, true)
   }
 
 
+  test("inf that works") {
+    //inf
+
+    assertEquals(inf(Cell.Number(1), Cell.Number(2)), Cell.Booleen(true))
+  }
+
+  test("inf number + text") {
+    //inf
+    assertEquals(inf(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+  }
+
+  test("supp that works") {
+    //supp
+
+    assertEquals(supp(Cell.Number(3), Cell.Number(2)), Cell.Booleen(true))
+  }
+
+  test("supp number + text") {
+    //inf
+    assertEquals(supp(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+  }
+
+  test("infouegale that works") {
+    //inf
+
+    assertEquals(infouegale(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
+  }
+
+  test("infouegale number + text") {
+    //inf
+    assertEquals(infouegale(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+  }
+
+  test("suppouegale that works") {
+    //inf
+
+    assertEquals(suppouegale(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
+  }
+
+  test("suppouegale number + text") {
+    //inf
+    assertEquals(suppouegale(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+  }
 }
 
 
