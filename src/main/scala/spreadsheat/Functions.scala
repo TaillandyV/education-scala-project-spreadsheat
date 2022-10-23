1a6c24b21c20ccbdfa9b471888a695512d066822
TaillandyV <valentin.taillandy@edu.esiee.fr>
2022-10-23 15:39:05 +0200

    Corrected function name and placed them in the correct file

--- src/main/scala/spreadsheat/Functions.scala
+++ src/main/scala/spreadsheat/Functions.scala
@@ -4,6 +4,64 @@ import spreadsheat.Cell.isCellNum
 import spreadsheat.Cell.isCellEmpty
 import spreadsheat.Cell
 
+def equals(l1 : Cell, l2: Cell): Boolean = l1.equals(l2)
+
+def inf(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 < a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
+
+def supp(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 > a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
+
+def infOrEqual(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 <= a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
+
+def supOrEqual(cell1: Cell, cell2: Cell): Cell =
+  if (isCellNum(cell1) && isCellNum(cell2)) {
+    val a1 = cell1.getNum.get
+    val a2 = cell2.getNum.get
+    Cell.Booleen(a1 >= a2)
+  }
+  else if (isCellEmpty(cell1)) {
+    Cell.Number(cell2.getNum.get)
+  }
+  else if (isCellEmpty(cell2)) {
+    Cell.Number(cell1.getNum.get)
+  }
+  else Cell.ErrorCell
+
 def sum(listOfCell: List[Cell]): Cell =
   var res : Double = 0
   listOfCell.zipWithIndex.foreach{
--- src/test/scala/spreadsheat/fonctions2suite.scala
+++ src/test/scala/spreadsheat/fonctions2suite.scala
@@ -7,64 +7,6 @@ import math.Ordering.Implicits.infixOrderingOps
 import munit.Clue.generate
 import spreadsheat.Cell.{isCellEmpty, isCellNum, stringToBooleen}
 
-def equals(l1 : Cell, l2: Cell): Boolean = l1.equals(l2)
-
-def inf(cell1: Cell, cell2: Cell): Cell =
-  if (isCellNum(cell1) && isCellNum(cell2)) {
-    val a1 = cell1.getNum.get
-    val a2 = cell2.getNum.get
-    Cell.Booleen(a1 < a2)
-  }
-  else if (isCellEmpty(cell1)) {
-    Cell.Number(cell2.getNum.get)
-  }
-  else if (isCellEmpty(cell2)) {
-    Cell.Number(cell1.getNum.get)
-  }
-  else Cell.ErrorCell
-
-def supp(cell1: Cell, cell2: Cell): Cell =
-  if (isCellNum(cell1) && isCellNum(cell2)) {
-    val a1 = cell1.getNum.get
-    val a2 = cell2.getNum.get
-    Cell.Booleen(a1 > a2)
-  }
-  else if (isCellEmpty(cell1)) {
-    Cell.Number(cell2.getNum.get)
-  }
-  else if (isCellEmpty(cell2)) {
-    Cell.Number(cell1.getNum.get)
-  }
-  else Cell.ErrorCell
-
-def infouegale(cell1: Cell, cell2: Cell): Cell =
-  if (isCellNum(cell1) && isCellNum(cell2)) {
-    val a1 = cell1.getNum.get
-    val a2 = cell2.getNum.get
-    Cell.Booleen(a1 <= a2)
-  }
-  else if (isCellEmpty(cell1)) {
-    Cell.Number(cell2.getNum.get)
-  }
-  else if (isCellEmpty(cell2)) {
-    Cell.Number(cell1.getNum.get)
-  }
-  else Cell.ErrorCell
-
-def suppouegale(cell1: Cell, cell2: Cell): Cell =
-  if (isCellNum(cell1) && isCellNum(cell2)) {
-    val a1 = cell1.getNum.get
-    val a2 = cell2.getNum.get
-    Cell.Booleen(a1 >= a2)
-  }
-  else if (isCellEmpty(cell1)) {
-    Cell.Number(cell2.getNum.get)
-  }
-  else if (isCellEmpty(cell2)) {
-    Cell.Number(cell1.getNum.get)
-  }
-  else Cell.ErrorCell
-
 class Functions2Suite extends munit.FunSuite {
 
   test("Equal") {
@@ -98,26 +40,26 @@ class Functions2Suite extends munit.FunSuite {
     assertEquals(supp(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
   }
 
-  test("infouegale that works") {
+  test("infOrEqual that works") {
     //inf
 
-    assertEquals(infouegale(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
+    assertEquals(infOrEqual(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
   }
 
-  test("infouegale number + text") {
+  test("infOrEqual number + text") {
     //inf
-    assertEquals(infouegale(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+    assertEquals(infOrEqual(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
   }
 
-  test("suppouegale that works") {
+  test("supOrEqual that works") {
     //inf
 
-    assertEquals(suppouegale(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
+    assertEquals(supOrEqual(Cell.Number(2), Cell.Number(2)), Cell.Booleen(true))
   }
 
-  test("suppouegale number + text") {
+  test("supOrEqual number + text") {
     //inf
-    assertEquals(suppouegale(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
+    assertEquals(supOrEqual(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
   }
 }
 
