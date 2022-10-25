8bb36962e7073303f7e09aeada77cd2e7567d71e
AdamGouja <adam.gouja@gmail.com>
2022-10-25 15:34:52 +0200

    Testing every function except max in FunctionsSuite

--- src/test/scala/spreadsheat/FunctionsSuite.scala
+++ src/test/scala/spreadsheat/FunctionsSuite.scala
@@ -8,6 +8,7 @@ import spreadsheat.Cell.isCellEmpty
 
 class FunctionsSuite extends munit.FunSuite {
 
+  //-------------------------------Operator "<"----------------------------------\\
   test("inf that works"){
     val cell1 = Cell.Number(1)
     val cell2 = Cell.Number(2)
@@ -24,6 +25,7 @@ class FunctionsSuite extends munit.FunSuite {
     assertEquals(compareOperator(cell2,"<", cell2), Cell.ErrorCell)
   }
 
+  //-------------------------------Operator ">"----------------------------------\\
   test("supp that works") {
     val cell1 = Cell.Number(3)
     val cell2 = Cell.Number(2)
@@ -41,6 +43,7 @@ class FunctionsSuite extends munit.FunSuite {
     assertEquals(compareOperator(cell2,"<", cell2), Cell.ErrorCell)
   }
 
+  //-------------------------------Operator "<="----------------------------------\\
   test("infOrEqual that works") {
     val cell1 = Cell.Number(1)
     val cell2 = Cell.Number(2)
@@ -59,6 +62,7 @@ class FunctionsSuite extends munit.FunSuite {
     assertEquals(compareOperator(cell2,"<", cell2), Cell.ErrorCell)
   }
 
+  //-------------------------------Operator ">="----------------------------------\\
   test("supOrEqual that works") {
     val cell1 = Cell.Number(1)
     val cell2 = Cell.Number(2)
@@ -77,119 +81,243 @@ class FunctionsSuite extends munit.FunSuite {
     assertEquals(compareOperator(cell2,"<", cell2), Cell.ErrorCell)
   }
 
+  //-------------------------------Function "sum"----------------------------------\\
   test("sum that works") {
-    val liste = List(Cell.Number(1), Cell.Empty, Cell.Number(3), Cell.Number(4))
-    assertEquals(sum(liste), Cell.Number(8))
+    val listOfCells = List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))
+
+    assertEquals(sum(listOfCells), Cell.Number(10))
   }
+
+  test("sum with empty") {
+    val listOfCells = List(Cell.Number(1), Cell.Empty, Cell.Number(3), Cell.Number(4))
+
+    assertEquals(sum(listOfCells), Cell.Number(8))
+  }
+
   test("sum with text") {
-    //sum
-    assertEquals(sum(List(Cell.Number(1), Cell.Empty, Cell.Text("3"), Cell.Number(4))), Cell.ErrorCell)
+    val listOfCells = List(Cell.Number(1), Cell.Empty, Cell.Text("3"), Cell.Number(4))
+
+    assertEquals(sum(listOfCells), Cell.ErrorCell)
+  }
+
+  //-------------------------------Operator "+"----------------------------------\\
+  test("addition that works") {
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Number(2)
+
+    assertEquals(calculateOperator(cell1, "+", cell2), Cell.Number(3))
+    assertEquals(calculateOperator(cell2, "+", cell1), Cell.Number(3))
+  }
+
+  test("addition number + empty") {
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Empty
+
+    assertEquals(calculateOperator(cell1, "+", cell2), Cell.Number(1))
+    assertEquals(calculateOperator(cell2, "+", cell1), Cell.Number(1))
   }
 
   test("addition number + text") {
-    //addition
-    assertEquals(addition(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(addition(Cell.Number(2), Cell.Text("ed")))
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "+", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "+", cell1), Cell.ErrorCell)
   }
+
   test("addition cell empty + text") {
-    //addition
-    assertEquals(addition(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(addition(Cell.Empty, Cell.Text("ed")))
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "+",cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "+",cell1), Cell.ErrorCell)
   }
-  test("addition that works") {
-    //addition
-    assertEquals(addition(Cell.Number(2), Cell.Number(5)), Cell.Number(7))
-    println(addition(Cell.Number(2), Cell.Number(5)))
+
+  //-------------------------------Operator "-"----------------------------------\\
+  test("soustraction that works") {
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Number(2)
+
+    assertEquals(calculateOperator(cell1, "-", cell2), Cell.Number(-1))
+    assertEquals(calculateOperator(cell2, "-", cell1), Cell.Number(1))
   }
+
+  test("soustraction number + empty") {
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Empty
+
+    assertEquals(calculateOperator(cell1, "-", cell2), Cell.Number(1))
+    assertEquals(calculateOperator(cell2, "-", cell1), Cell.Number(-1))
+  }
+
   test("soustraction number + text") {
-    //soustraction
-    assertEquals(minus(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(minus(Cell.Number(2), Cell.Text("ed")))
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "-", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "-", cell1), Cell.ErrorCell)
   }
   test("soustraction cell empty + text") {
-    //soustraction
-    assertEquals(minus(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(minus(Cell.Empty, Cell.Text("ed")))
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "-", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "-", cell1), Cell.ErrorCell)
   }
-  test("soustraction that works") {
-    //soustraction
-    assertEquals(minus(Cell.Number(-2), Cell.Number(-5)), Cell.Number(3))
-    println(minus(Cell.Number(-2), Cell.Number(-5)))
+
+  //-------------------------------Operator "*"----------------------------------\\
+  test("multiplication that works") {
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Number(4)
+
+    assertEquals(calculateOperator(cell1, "*", cell2), Cell.Number(12))
+    assertEquals(calculateOperator(cell2, "*", cell1), Cell.Number(12))
+  }
+
+  test("multiplication number + empty") {
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Empty
+
+    assertEquals(calculateOperator(cell1, "*", cell2), Cell.Number(0))
+    assertEquals(calculateOperator(cell2, "*", cell1), Cell.Number(0))
   }
 
   test("multiplication number + text") {
-    //multiplication
-    assertEquals(multiplication(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(multiplication(Cell.Number(2), Cell.Text("ed")))
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Text("4")
+
+    assertEquals(calculateOperator(cell1, "*", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "*", cell1), Cell.ErrorCell)
   }
+
   test("multiplication cell empty + text") {
-    //multiplication
-    assertEquals(multiplication(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(multiplication(Cell.Empty, Cell.Text("ed")))
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Text("4")
+
+    assertEquals(calculateOperator(cell1, "*", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "*", cell1), Cell.ErrorCell)
   }
-  test("multiplication that works") {
-    //multiplication
-    assertEquals(multiplication(Cell.Number(-2), Cell.Number(1)), Cell.Number(-2))
-    println(multiplication(Cell.Number(-2), Cell.Number(1)))
+
+  //-------------------------------Operator "/"----------------------------------\\
+  test("division that works") {
+    val cell1 = Cell.Number(4)
+    val cell2 = Cell.Number(2)
+
+    assertEquals(calculateOperator(cell1, "/", cell2), Cell.Number(2))
+    assertEquals(calculateOperator(cell2, "/", cell1), Cell.Number(0.5))
   }
 
   test("division number + text") {
-    //division
-    assertEquals(division(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(division(Cell.Number(2), Cell.Text("ed")))
+    val cell1 = Cell.Number(4)
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "/", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "/", cell1), Cell.ErrorCell)
   }
-  test("division cell empty + text") {
-    //division
-    assertEquals(division(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(division(Cell.Empty, Cell.Text("ed")))
+
+  test("division empty + text") {
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Text("2")
+
+    assertEquals(calculateOperator(cell1, "/", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "/", cell1), Cell.ErrorCell)
   }
-  test("division number/0") {
-    //division
-    assertEquals(division(Cell.Number(0), Cell.Number(0)), Cell.ErrorCell)
-    println(division(Cell.Number(0), Cell.Number(0)))
+
+  test("division number + 0") {
+    val cell1 = Cell.Number(4)
+    val cell2 = Cell.Number(0)
+
+    assertEquals(calculateOperator(cell1, "/", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "/", cell1), Cell.Number(0))
   }
-  test("division that works") {
-    //division
-    assertEquals(division(Cell.Number(-0), Cell.Number(0)), Cell.Number(0))
-    println(division(Cell.Number(-0), Cell.Number(0)))
+  test("division number + empty") {
+    val cell1 = Cell.Number(4)
+    val cell2 = Cell.Empty
+
+    assertEquals(calculateOperator(cell1, "/", cell2), Cell.ErrorCell)
+    assertEquals(calculateOperator(cell2, "/", cell1), Cell.Number(0))
+  }
+
+  //-------------------------------Function "concat"----------------------------------\\
+  test("concat text + text") {
+    val cell1 = Cell.Text("Hello")
+    val cell2 = Cell.Text("World")
+
+    assertEquals(concat(cell1, cell2), Cell.Text("HelloWorld"))
+    assertEquals(concat(cell2, cell1), Cell.Text("WorldHello"))
   }
 
   test("concat text + number") {
-    //concat
-    assertEquals(concat(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(concat(Cell.Number(2), Cell.Empty))
-  }
-  test("concat cell empty + number") {
-    //division
-    assertEquals(concat(Cell.Number(2), Cell.Text("ed")), Cell.ErrorCell)
-    println(concat(Cell.Empty, Cell.Number(2)))
-  }
-  test("concat that works") {
-    //concat
-    assertEquals(concat(Cell.Text("hello"), Cell.Text("world")), Cell.Text("hello world"))
-    println(concat(Cell.Empty, Cell.Text("world")))
+    val cell1 = Cell.Text("Hello")
+    val cell2 = Cell.Number(1)
+
+    assertEquals(concat(cell1, cell2), Cell.ErrorCell)
+    assertEquals(concat(cell2, cell1), Cell.ErrorCell)
   }
 
+  test("concat empty + number") {
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Number(1)
+
+    assertEquals(concat(cell1, cell2), Cell.Number(1))
+    assertEquals(concat(cell2, cell1), Cell.Number(1))
+  }
+
+  test("concat empty + text") {
+    val cell1 = Cell.Empty
+    val cell2 = Cell.Text("World")
+
+    assertEquals(concat(cell1, cell2), Cell.Text("World"))
+    assertEquals(concat(cell2, cell1), Cell.Text("World"))
+  }
+
+  test("concat number + number") {
+    val cell1 = Cell.Number(1)
+    val cell2 = Cell.Number(2)
+
+    assertEquals(concat(cell1, cell2), Cell.Number(12))
+    assertEquals(concat(cell2, cell1), Cell.Number(21))
+  }
+
+  //-------------------------------Function "minimum"----------------------------------\\
+  test("minimum with only numbers") {
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Number(2)
+    val cell3 = Cell.Number(1)
+    val cell4 = Cell.Number(4)
+
+    val listOfCells = List(cell1, cell2, cell3, cell4)
+    assertEquals(min(listOfCells), Cell.Number(1))
+  }
   test("minimum with text") {
-    //minimum
-    assertEquals(minimum(List(Cell.Number(1), Cell.Empty, Cell.Text("3"), Cell.Number(4))), Cell.ErrorCell)
-    println(minimum(List(Cell.Number(1), Cell.Number(2), Cell.Text("3"), Cell.Number(4))))
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Text("2")
+    val cell3 = Cell.Number(1)
+    val cell4 = Cell.Number(4)
+
+    val listOfCells = List(cell1, cell2, cell3, cell4)
+    assertEquals(min(listOfCells), Cell.ErrorCell)
   }
-  test("minimum that works") {
-    //minimum
-    assertEquals(minimum(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))), Cell.Number(1))
-    println(minimum(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))))
+  test("minimum with empty") {
+    val cell1 = Cell.Number(3)
+    val cell2 = Cell.Empty
+    val cell3 = Cell.Number(2)
+    val cell4 = Cell.Number(4)
+
+    val listOfCells = List(cell1, cell2, cell3, cell4)
+    assertEquals(min(listOfCells), Cell.Number(2))
   }
+
 
   test("maximum with text") {
     //maximum
-    assertEquals(maximum(List(Cell.Number(1), Cell.Empty, Cell.Text("3"), Cell.Number(4))), Cell.ErrorCell)
-    println(maximum(List(Cell.Number(1), Cell.Number(2), Cell.Text("3"), Cell.Number(4))))
+    assertEquals(max(List(Cell.Number(1), Cell.Empty, Cell.Text("3"), Cell.Number(4))), Cell.ErrorCell)
+    println(max(List(Cell.Number(1), Cell.Number(2), Cell.Text("3"), Cell.Number(4))))
   }
   test("maximum that works") {
     //maximum
-    assertEquals(maximum(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))), Cell.Number(4))
-    println(maximum(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))))
+    assertEquals(max(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))), Cell.Number(4))
+    println(max(List(Cell.Number(1), Cell.Number(2), Cell.Number(3), Cell.Number(4))))
   }
- 
+
 }
